f98552f613b7239ad94a48dadea2a53b3426ae31
Sergey Khabibullin <sergey@khabibullin.com>
2017-06-08 12:42:10 +0300

    Add basic file explorer

---
+++ Restofus/Components/Files/ReactiveFile.cs
@@ -0,0 +1,38 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restofus.Components.Files
+{
+    public class ReactiveFile : ReactiveObject
+    {
+        string path;
+        public string Path
+        {
+            get => path;
+            set => this.RaiseAndSetIfChanged(ref path, value);
+        }
+
+        string name;
+        public string Name
+        {
+            get => name;
+            set => this.RaiseAndSetIfChanged(ref name, value);
+        }
+
+        bool isDirectory;
+        public bool IsDirectory
+        {
+            get => isDirectory;
+            set => this.RaiseAndSetIfChanged(ref isDirectory, value);
+        }
+
+        ReactiveFileCollection files;
+        public ReactiveFileCollection Files
+        {
+            get => files;
+            set => this.RaiseAndSetIfChanged(ref files, value);
+        }
+    }
+}
--- Restofus/Pads/NavigationPad.xaml.cs
+++ Restofus/Pads/NavigationPad.xaml.cs
@@ -3,8 +3,12 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using ReactiveUI;
 using Restofus.Components;
+using Restofus.Components.Files;
 using Restofus.Utils;
 using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Restofus.Pads
 {
@@ -20,10 +24,51 @@ namespace Restofus.Pads
             public Context(
                 Navigator navigator)
             {
-                Task.Run(async () => {
+                RootFiles = new ReactiveFileCollection();
+                RootFiles.Add(BuildFileTree("C:\\rest"));
+
+                Task.Run(async () =>
+                {
                     await Task.Delay(5000);
                     await navigator.Navigate(null);
                 });
+            }
+
+            public ReactiveFileCollection RootFiles { get; }
+
+            ReactiveFile BuildFileTree(string path)
+            {
+                ReactiveFile file = null;
+
+                if (Directory.Exists(path))
+                {
+                    file = new ReactiveFile
+                    {
+                        Path = new DirectoryInfo(path).FullName,
+                        Name = new DirectoryInfo(path).Name,
+                        IsDirectory = true,
+                        Files = new ReactiveFileCollection()
+                    };
+
+                    var children = Enumerable.Concat(
+                        Directory.EnumerateDirectories(path),
+                        Directory.EnumerateFiles(path));
+
+                    foreach (var child in children)
+                    {
+                        file.Files.Add(BuildFileTree(child));
+                    }
+                }
+                else if (File.Exists(path))
+                {
+                    file = new ReactiveFile
+                    {
+                        Path = new FileInfo(path).FullName,
+                        Name = new FileInfo(path).Name
+                    };
+                }
+
+                return file;
             }
         }
     }
